47fb93b3733da5f9b63dd6792db03713390d1fdf
ChiaraR3 <chiara.r.3@live.it>
2021-09-13 19:17:19 +0000

    pasa todo a front

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -18,6 +19,11 @@ export const Home = () => {
 				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
 					Read documentation
 				</a>
+				<Link to="/login">
+					<button type="button" className="btn btn-primary">
+						Login
+					</button>
+				</Link>
 			</p>
 		</div>
 	);
